573e0e09ba10ecde8be754d45f01620766aa3d04
Stanislav Pidik <stanislav.pidik@stud.fh-campuswien.ac.at>
2021-03-18 18:56:57 +0100

    return grade without round up

--- src/main/java/Result.java
+++ src/main/java/Result.java
@@ -1,9 +1,41 @@
+import java.util.ArrayList;
 import java.util.List;
+
+
 
 public class Result {
 
+    private static final int MINIMAL_GRADE = 40-2;// minimum to round up
+
+
     public static List<Integer> gradingStudents(List<Integer> grades){
 
-        return grades;
+        List<Integer> gradesToReturn = new ArrayList<>();
+
+        for (int i = 1; i < grades.size(); i++) {
+
+
+
+            int grade = grades.get(i);
+
+            int next = 0;
+            int mod = 0;
+            int tens = 0;
+
+            if (grade >= MINIMAL_GRADE) {
+
+            }
+
+            gradesToReturn.add(grade);
+        }
+
+        return gradesToReturn;
     }
+
+    public static void main(String[] args) {
+
+
+    }
+
+
 }
