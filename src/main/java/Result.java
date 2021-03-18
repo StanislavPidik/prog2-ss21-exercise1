55f4e4d6b010aa2a5c8e5525f25659ad02f54a5b
Stanislav Pidik <stanislav.pidik@stud.fh-campuswien.ac.at>
2021-03-18 19:40:29 +0100

    implemented check for student count

--- src/main/java/Result.java
+++ src/main/java/Result.java
@@ -8,43 +8,45 @@ public class Result {
     private static final int LIMIT_ROUND_UP = 3;// limit to round up
     private static final int MINIMAL_GRADE = 0;// limit to round up
     private static final int MAXIMAL_GRADE = 100;// limit to round up
+    private static final int MINIMAL_GRADE_COUNT = 1;// limit to round up
+    private static final int MAXIMAL_GRADE_COUNT = 60;// limit to round up
 
     public static List<Integer> gradingStudents(List<Integer> grades) {
 
         List<Integer> gradesToReturn = new ArrayList<>();
 
-        for (int i = 1; i < grades.size(); i++) {
+        if (grades.get(0) == (grades.size() - 1) && grades.get(0) >= MINIMAL_GRADE_COUNT && grades.get(0) <= MAXIMAL_GRADE_COUNT) {
+            for (int i = 1; i < grades.size(); i++) {
 
 
-            int grade = grades.get(i);
+                int grade = grades.get(i);
+                int next = 0;
+                int mod = 0;
+                int tens = 0;
 
-            int next = 0;
-            int mod = 0;
-            int tens = 0;
-
-            if (grade >= MINIMAL_GRADE && grade <= MAXIMAL_GRADE) {
-                if (grade >= MINIMAL_GRADE_FOR_ROUND) {
-
-                    mod = grade % 10;
-                    tens = grade - mod;
-
-                    if (mod > 5) {
-                        next = tens + 10;
-                    } else {
-                        next = tens + 5;
+                if (grade >= MINIMAL_GRADE && grade <= MAXIMAL_GRADE) {
+                    if (grade >= MINIMAL_GRADE_FOR_ROUND) {
+                        mod = grade % 10;
+                        tens = grade - mod;
+                        if (mod > 5) {
+                            next = tens + 10;
+                        } else {
+                            next = tens + 5;
+                        }
+                        if (next - grade < LIMIT_ROUND_UP) {
+                            grade += next - grade;
+                        }
                     }
-
-                    if (next - grade < LIMIT_ROUND_UP) {
-                        grade += next - grade;
-                    }
-
+                } else {
+                    System.out.println("Wrong grade(s) inside the list - grade list wil be returned without change!");
+                    return grades;
                 }
-            } else {
-                System.out.println("Wrong grade(s) inside the list - grade list wil be returned without change!");
-                return grades;
-            }
 
-            gradesToReturn.add(grade);
+                gradesToReturn.add(grade);
+            }
+        } else {
+            System.out.println("Wrong student count - grade list wil be returned without change!");
+            return grades;
         }
 
         return gradesToReturn;
